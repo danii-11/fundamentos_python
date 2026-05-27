76a61d04e639c18b59caa7085ab640e9a406a057
Daniela Rodriguez <daniiela.rodriguez112@gmail.com>
2026-05-27 07:50:54 -0500

    modulos

---
+++ modulos.py
@@ -0,0 +1,8 @@
+from funciones import suma, resta, multiplicacion, division, saludar
+
+print(suma(5,8))
+print(resta(10,4))
+print(multiplicacion(2,3))
+print(division(10,2))
+
+print(saludar("Daniela", "Rodriguez"))
